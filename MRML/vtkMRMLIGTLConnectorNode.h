efa2c7345f50080573909ae2020bbc5c5e2b1893
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-04-07 22:04:54 +0000

    ENH: adding MRML nodes and IGTL to MRML converters. CMakeLists.txt was updated to link the module with OpenIGTLink library.
    git-svn-id: http://svn.na-mic.org/NAMICSandBox/trunk/IGTLoadableModules/QtModules/OpenIGTLinkIF@7420 5e132c66-7cf4-0310-b4ca-cd4a7079c2d8

---
+++ MRML/vtkMRMLIGTLConnectorNode.h
@@ -0,0 +1,388 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2009 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLCurveAnalysisNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+#ifndef __vtkMRMLIGTLConnectorNode_h
+#define __vtkMRMLIGTLConnectorNode_h
+
+#include "qSlicerOpenIGTLinkIFModuleExport.h"
+
+#include "vtkMRML.h"
+#include "vtkMRMLNode.h"
+#include "vtkMRMLStorageNode.h"
+
+#include "vtkIGTLToMRMLBase.h"
+
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+
+#include "vtkObject.h"
+#include "igtlServerSocket.h"
+#include "igtlClientSocket.h"
+
+#ifdef OpenIGTLinkIF_USE_VERSION_2
+  #include "vtkMRMLIGTLQueryNode.h"
+#endif //OpenIGTLinkIF_USE_VERSION_2
+
+class vtkMultiThreader;
+class vtkMutexLock;
+class vtkIGTLCircularBuffer;
+
+class Q_SLICER_QTMODULES_OPENIGTLINKIF_EXPORT vtkMRMLIGTLConnectorNode : public vtkMRMLNode
+{
+
+ public:
+
+  //----------------------------------------------------------------
+  // Constants Definitions
+  //----------------------------------------------------------------
+
+  //BTX
+  // Events
+  enum {
+    ConnectedEvent        = 118944,
+    DisconnectedEvent     = 118945,
+    ActivatedEvent        = 118946,
+    DeactivatedEvent      = 118947,
+    ReceiveEvent          = 118948,
+    NewDeviceEvent        = 118949,
+  };
+
+  enum {
+    TYPE_NOT_DEFINED,
+    TYPE_SERVER,
+    TYPE_CLIENT,
+    NUM_TYPE
+  };
+  
+  enum {
+    STATE_OFF,
+    STATE_WAIT_CONNECTION,
+    STATE_CONNECTED,
+    NUM_STATE
+  };
+
+  enum {
+    IO_UNSPECIFIED = 0x00,
+    IO_INCOMING   = 0x01,
+    IO_OUTGOING   = 0x02,
+  };
+
+  typedef struct {
+    std::string   name;
+    std::string   type;
+    int           io;
+    //vtkMRMLNode*  node;
+  } DeviceInfoType;
+
+  typedef struct {
+    vtkMRMLNode*  node;
+    int           lock;
+    int           second;
+    int           nanosecond;
+  } NodeInfoType;
+
+  typedef std::map<int, DeviceInfoType>   DeviceInfoMapType;   // Device list:  index is referred as
+                                                               // a device id in the connector.
+  typedef std::set<int>                   DeviceIDSetType;
+  typedef std::list<vtkIGTLToMRMLBase*>   MessageConverterListType;
+  typedef std::vector<vtkMRMLNode*>       MRMLNodeListType;
+  typedef std::vector<NodeInfoType>       NodeInfoListType;
+  typedef std::map<std::string, vtkIGTLToMRMLBase*> MessageConverterMapType;
+  //ETX
+
+ public:
+
+  //----------------------------------------------------------------
+  // Standard methods for MRML nodes
+  //----------------------------------------------------------------
+
+  static vtkMRMLIGTLConnectorNode *New();
+  vtkTypeMacro(vtkMRMLIGTLConnectorNode,vtkMRMLNode);
+  
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName()
+    {return "IGTLConnector";};
+
+  // method to propagate events generated in mrml
+  virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
+
+ protected:
+  //----------------------------------------------------------------
+  // Constructor and destroctor
+  //----------------------------------------------------------------
+  
+  vtkMRMLIGTLConnectorNode();
+  ~vtkMRMLIGTLConnectorNode();
+  vtkMRMLIGTLConnectorNode(const vtkMRMLIGTLConnectorNode&);
+  void operator=(const vtkMRMLIGTLConnectorNode&);
+
+
+ public:
+  //----------------------------------------------------------------
+  // Connector configuration
+  //----------------------------------------------------------------
+
+  vtkGetMacro( ServerPort, int );
+  vtkSetMacro( ServerPort, int );
+  vtkGetMacro( Type, int );
+  vtkSetMacro( Type, int );
+  vtkGetMacro( State, int );
+  //vtkSetMacro( State, int );
+  vtkSetMacro( RestrictDeviceName, int );
+  vtkGetMacro( RestrictDeviceName, int );
+
+  // host name
+  void SetServerHostname(const char* str) { this->ServerHostname = str; }
+  const char* GetServerHostname() { return this->ServerHostname.c_str(); }
+  //BTX
+  void SetServerHostname(std::string str) { this->ServerHostname = str; }
+  //ETX
+
+  int SetTypeServer(int port);
+  int SetTypeClient(char* hostname, int port);
+  //BTX
+  int SetTypeClient(std::string hostname, int port);
+  //ETX
+
+  vtkGetMacro( CheckCRC, int );
+  void SetCheckCRC(int c);
+
+  
+  //----------------------------------------------------------------
+  // Thread Control
+  //----------------------------------------------------------------
+
+  int Start();
+  int Stop();
+  static void* ThreadFunction(void* ptr);
+
+  //----------------------------------------------------------------
+  // OpenIGTLink Message handlers
+  //----------------------------------------------------------------
+  int WaitForConnection();
+  int ReceiveController();
+  int SendData(int size, unsigned char* data);
+  int Skip(int length, int skipFully=1);
+
+  //----------------------------------------------------------------
+  // Circular Buffer
+  //----------------------------------------------------------------
+
+  //BTX
+  typedef std::vector<std::string> NameListType;
+  unsigned int GetUpdatedBuffersList(NameListType& nameList); // TODO: this will be moved to private
+  vtkIGTLCircularBuffer* GetCircularBuffer(std::string& key);     // TODO: Is it OK to use device name as a key?
+  //ETX
+
+  //----------------------------------------------------------------
+  // Device Lists
+  //----------------------------------------------------------------
+
+  // Description:
+  // Import received data from the circular buffer to the MRML scne.
+  // This is currently called by vtkOpenIGTLinkIFLogic class.
+  void ImportDataFromCircularBuffer();
+
+  // Description:
+  // Import events from the event buffer to the MRML scene.
+  // This is currently called by vtkOpenIGTLinkIFLogic class.
+  void ImportEventsFromEventBuffer();
+
+  // Description:
+  // Register IGTL to MRML message converter.
+  // This is used by vtkOpenIGTLinkIFLogic class.
+  int RegisterMessageConverter(vtkIGTLToMRMLBase* converter);
+
+  // Description:
+  // Unregister IGTL to MRML message converter.
+  // This is used by vtkOpenIGTLinkIFLogic class.
+  void UnregisterMessageConverter(vtkIGTLToMRMLBase* converter);
+  
+  // Description:
+  // Set and start observing MRML node for outgoing data.
+  // If devType == NULL, a converter is chosen based only on MRML Tag.
+  int RegisterOutgoingMRMLNode(vtkMRMLNode* node, const char* devType=NULL);
+
+  // Description:
+  // Stop observing and remove MRML node for outgoing data.
+  void UnregisterOutgoingMRMLNode(vtkMRMLNode* node);
+
+  // Description:
+  // Register MRML node for incoming data.
+  // Returns a pointer to the node information in IncomingMRMLNodeInfoList
+  NodeInfoType* RegisterIncomingMRMLNode(vtkMRMLNode* node);
+
+  // Description:
+  // Unregister MRML node for incoming data.
+  void UnregisterIncomingMRMLNode(vtkMRMLNode* node);
+
+  // Description:
+  // Get number of registered outgoing MRML nodes:
+  unsigned int GetNumberOfOutgoingMRMLNodes();
+
+  // Description:
+  // Get Nth outgoing MRML nodes:
+  vtkMRMLNode* GetOutgoingMRMLNode(unsigned int i);
+
+  // Description:
+  // Get MRML to IGTL converter assigned to the specified MRML node ID
+  vtkIGTLToMRMLBase* GetConverterByNodeID(const char* id);
+
+  // Description:
+  // Get number of registered outgoing MRML nodes:
+  unsigned int GetNumberOfIncomingMRMLNodes();
+
+  // Description:
+  // Get Nth outgoing MRML nodes:
+  vtkMRMLNode* GetIncomingMRMLNode(unsigned int i);
+  
+  // Description:
+  // A function to explicitly push node to OpenIGTLink
+  // (Usually, data stored in MRML scene are exported, when the registered events are invoked.)
+  void PushNode(vtkMRMLNode* node);
+
+#ifdef OpenIGTLinkIF_USE_VERSION_2
+  // Description:
+  // Push query int the query list.
+  //BTX
+  void PushQuery(vtkMRMLIGTLQueryNode* query);
+  //ETX
+#endif //OpenIGTLinkIF_USE_VERSION_2
+
+  //----------------------------------------------------------------
+  // For OpenIGTLink time stamp access
+  //----------------------------------------------------------------
+
+  // Description:
+  // Turn lock flag on to stop updating MRML node. Call this function before
+  // reading the content of the MRML node and the corresponding time stamp.
+  void LockIncomingMRMLNode(vtkMRMLNode* node);
+
+  // Description:
+  // Turn lock flag off to start updating MRML node. Make sure to call this function
+  // after reading the content / time stamp.
+  void UnlockIncomingMRMLNode(vtkMRMLNode* node);
+
+  // Description:
+  // Get OpenIGTLink's time stamp information. Returns 0, if it fails to obtain time stamp.
+  int GetIGTLTimeStamp(vtkMRMLNode* node, int& second, int& nanosecond);
+
+
+ private:
+
+  vtkIGTLToMRMLBase* GetConverterByMRMLTag(const char* tag);
+  vtkIGTLToMRMLBase* GetConverterByIGTLDeviceType(const char* type);
+
+  // Description:
+  // Inserts the eventId to the EventQueue, and the event will be invoked from the main thread
+  void RequestInvokeEvent(unsigned long eventId);
+
+ private:
+  //----------------------------------------------------------------
+  // Connector configuration 
+  //----------------------------------------------------------------
+  //BTX
+  std::string Name;
+  //ETX
+  int Type;
+  int State;
+
+  //----------------------------------------------------------------
+  // Thread and Socket
+  //----------------------------------------------------------------
+
+  vtkMultiThreader* Thread;
+  vtkMutexLock*     Mutex;
+  //BTX
+  igtl::ServerSocket::Pointer  ServerSocket;
+  igtl::ClientSocket::Pointer  Socket;
+  //ETX
+  int               ThreadID;
+  int               ServerPort;
+  int               ServerStopFlag;
+
+  //BTX
+  std::string       ServerHostname;
+  //ETX
+
+  //----------------------------------------------------------------
+  // Data
+  //----------------------------------------------------------------
+
+
+  //BTX
+  typedef std::map<std::string, vtkIGTLCircularBuffer*> CircularBufferMap;
+  CircularBufferMap Buffer;
+  //ETX
+
+  vtkMutexLock* CircularBufferMutex;
+  int           RestrictDeviceName;  // Flag to restrict incoming and outgoing data by device names
+
+  //BTX
+  // Event queueing mechanism is needed to send all event notifications from the main thread.
+  // Events can be pushed to the end of the EventQueue by calling RequestInvoke from any thread,
+  // and they will be Invoked in the main thread.
+  std::list<unsigned long> EventQueue;
+  vtkMutexLock* EventQueueMutex;
+  //ETX
+
+#ifdef OpenIGTLinkIF_USE_VERSION_2
+  // Query queueing mechanism is needed to send all queries from the connector thread.
+  // Queries can be pushed to the end of the QueryQueue by calling RequestInvoke from any thread,
+  // and they will be Invoked in the main thread.
+  //BTX
+  std::list<vtkMRMLIGTLQueryNode*> QueryWaitingQueue;
+  vtkMutexLock* QueryQueueMutex;
+  //ETX
+#endif //OpenIGTLinkIF_USE_VERSION_2
+
+
+  // -- Device Name (same as MRML node) and data type (data type string defined in OpenIGTLink)
+  DeviceIDSetType   IncomingDeviceIDSet;
+  DeviceIDSetType   OutgoingDeviceIDSet;
+  DeviceIDSetType   UnspecifiedDeviceIDSet;
+  
+  // Message converter (IGTLToMRML)
+  MessageConverterListType MessageConverterList;
+  MessageConverterMapType  IGTLNameToConverterMap;
+  MessageConverterMapType  MRMLIDToConverterMap;
+
+  // List of nodes that this connector node is observing.
+  MRMLNodeListType         OutgoingMRMLNodeList;
+  NodeInfoListType         IncomingMRMLNodeInfoList;
+  
+  int CheckCRC;
+  
+};
+
+#endif
+
