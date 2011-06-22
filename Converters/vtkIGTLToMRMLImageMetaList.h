455a60cbf3b8d9470c6f8db17b0ca943740f0e6a
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-06-22 16:10:20 +0000

    COMP: chaning directory structure
    git-svn-id: http://svn.na-mic.org/NAMICSandBox/trunk/IGTLoadableModules/QtModules/OpenIGTLinkIF@7580 5e132c66-7cf4-0310-b4ca-cd4a7079c2d8

---
+++ Converters/vtkIGTLToMRMLImageMetaList.h
@@ -0,0 +1,71 @@
+/*==========================================================================
+
+  Portions (c) Copyright 2008-2009 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $HeadURL: http://svn.slicer.org/Slicer3/trunk/Modules/OpenIGTLinkIF/vtkIGTLToMRMLImageMetaList.h $
+  Date:      $Date: 2009-08-12 21:30:38 -0400 (Wed, 12 Aug 2009) $
+  Version:   $Revision: 10236 $
+
+==========================================================================*/
+
+#ifndef __vtkIGTLToMRMLImageMetaList_h
+#define __vtkIGTLToMRMLImageMetaList_h
+
+//#include "qSlicerOpenIGTLinkIFModuleExport.h"
+#include "vtkSlicerOpenIGTLinkIFModuleLogicExport.h"
+
+#include "vtkObject.h"
+#include "vtkMRMLNode.h"
+#include "vtkIGTLToMRMLBase.h"
+
+#ifdef OpenIGTLinkIF_USE_VERSION_2
+ #include "igtlImageMetaMessage.h"
+#endif // OpenIGTLinkIF_USE_VERSION_2
+
+class vtkMRMLVolumeNode;
+
+class Q_SLICER_QTMODULES_OPENIGTLINKIF_EXPORT vtkIGTLToMRMLImageMetaList : public vtkIGTLToMRMLBase
+{
+ public:
+
+  static vtkIGTLToMRMLImageMetaList *New();
+  vtkTypeRevisionMacro(vtkIGTLToMRMLImageMetaList,vtkObject);
+
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual const char*  GetIGTLName() { return "IMGMETA"; };
+  virtual const char*  GetMRMLName() { return "ImageMetaList"; };
+
+  virtual vtkIntArray* GetNodeEvents();
+  virtual vtkMRMLNode* CreateNewNode(vtkMRMLScene* scene, const char* name);
+
+  //BTX
+  virtual int          IGTLToMRML(igtl::MessageBase::Pointer buffer, vtkMRMLNode* node);
+  //ETX
+  virtual int          MRMLToIGTL(unsigned long event, vtkMRMLNode* mrmlNode, int* size, void** igtlMsg);
+
+
+ protected:
+  vtkIGTLToMRMLImageMetaList();
+  ~vtkIGTLToMRMLImageMetaList();
+
+  void CenterImage(vtkMRMLVolumeNode *volumeNode);
+
+ protected:
+
+#ifdef OpenIGTLinkIF_USE_VERSION_2
+  //BTX
+  //igtl::TransformMessage::Pointer OutTransformMsg;
+  igtl::ImageMetaMessage::Pointer OutImageMetaMsg;
+  igtl::GetImageMetaMessage::Pointer GetImageMetaMessage;
+  //ETX
+#endif // OpenIGTLinkIF_USE_VERSION_2
+  
+};
+
+
+#endif //__vtkIGTLToMRMLImageMetaList_h
