d5ea9f2b48fe32f2dfbbfe1f60bd562856784338
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-12-14 16:33:40 +0000

    STYLE: Size of text fields n the connector property widget are adjusted. NOTE that The commits from r7803 to r7829 are contributed by Jean-Christophe Fillion-Robin, Kitware, Inc.
    git-svn-id: http://svn.na-mic.org/NAMICSandBox/trunk/IGTLoadableModules/QtModules/OpenIGTLinkIF@7835 5e132c66-7cf4-0310-b4ca-cd4a7079c2d8

--- qSlicerOpenIGTLinkIFModule.cxx
+++ qSlicerOpenIGTLinkIFModule.cxx
@@ -70,6 +70,7 @@ QString qSlicerOpenIGTLinkIFModule::acknowledgementText()const
          "NIH (R01CA111288, P41RR019703, P01CA067165 and U54EB005149) and NEDO, Japan.";
 }
 
+
 //-----------------------------------------------------------------------------
 QIcon qSlicerOpenIGTLinkIFModule::icon()const
 {
