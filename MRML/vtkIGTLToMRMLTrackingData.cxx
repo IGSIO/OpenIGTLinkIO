18754ca773cfd525fb7449176ed59c2d4cb15eed
Adam Rankin <adam.rankin@gmail.com>
2014-03-18 15:01:48 -0400

    Repariring whitespace

--- MRML/vtkIGTLToMRMLTrackingData.cxx
+++ MRML/vtkIGTLToMRMLTrackingData.cxx
@@ -58,6 +58,7 @@ void vtkIGTLToMRMLTrackingData::PrintSelf(ostream& os, vtkIndent indent)
 //---------------------------------------------------------------------------
 vtkMRMLNode* vtkIGTLToMRMLTrackingData::CreateNewNode(vtkMRMLScene* scene, const char* name)
 {
+
   vtkMRMLIGTLTrackingDataBundleNode *node = vtkMRMLIGTLTrackingDataBundleNode::New();
   node->SetName(name);
   node->SetDescription("Received by OpenIGTLink");
