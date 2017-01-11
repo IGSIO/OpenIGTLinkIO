b729caf1befa6713abb89a279710bf7524aad390
Simon Drouin <drouin.simon@gmail.com>
2017-01-10 23:53:03 -0500

    Missing files from last commit.

--- Tools/vtkIGTLIOCommandMessageCodec.h
+++ Tools/vtkIGTLIOCommandMessageCodec.h
@@ -1,11 +1,35 @@
-#ifndef VTKIGTLIOCOMMANDMESSAGECODEC_H
-#define VTKIGTLIOCOMMANDMESSAGECODEC_H
+#ifndef __vtkIGTLIOCommandMessageCodec_h_
+#define __vtkIGTLIOCommandMessageCodec_h_
 
+#include <vector>
+#include <utility>
 
-class vtkIGTLIOCommandMessageCodec : public vtkObject
+class vtkIGTLIOCommandMessageCodec
 {
+
 public:
+
     vtkIGTLIOCommandMessageCodec();
+    vtkIGTLIOCommandMessageCodec( bool isReply );
+
+    // Build the content of the command to send
+    void SetResult( bool res );
+    void SetParameter( std::string paramName, std::string value );
+    std::string GetContent();
+
+    // Parse the content of a command received
+    void SetContent( std::string content );
+    int GetNumberOfParameters() { return m_parameters.size(); }
+    bool IsReply() { return m_isReply; }
+    bool GetResult();
+    std::string GetParameter( std::string paramName );
+
+protected:
+
+    typedef std::vector<std::pair<std::string,std::string> > ParamContainer;
+    bool m_isReply;
+    bool m_result;
+    ParamContainer m_parameters;
 };
 
-#endif // VTKIGTLIOCOMMANDMESSAGECODEC_H
+#endif
