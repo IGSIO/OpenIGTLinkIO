867a0f61cb2e5709bf9527660d984791233774e2
Janne Beate Lervik Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2017-02-28 15:11:28 +0100

    When a image is received, an receiveEvent is invoked.

--- Devices/igtlioImageDevice.cxx
+++ Devices/igtlioImageDevice.cxx
@@ -75,6 +75,9 @@ int ImageDevice::ReceiveIGTLMessage(igtl::MessageBase::Pointer buffer, bool chec
  if (ImageConverter::fromIGTL(buffer, &HeaderData, &Content, checkCRC))
    {
    this->Modified();
+     std::cout << "ReceiveIGTLMessage " << std::endl;
+   this->InvokeEvent(ReceiveEvent, NULL);
+
    return 1;
    }
 
