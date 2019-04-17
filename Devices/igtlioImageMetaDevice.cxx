IGSIO/OpenIGTLinkIO

commit 53928bb559d4fb78c0e724b90f858cb7f8c2dc1f

Add IMGMETA, LBMETA and POINT devices and enable GET prefix for IMAGE, IMGMETA, LBMETA, and POINT

Devices/igtlioImageMetaDevice.cxx
@@ -1,0 +1,132 @@
+
+// OpenIGTLinkIO includes
+#include "igtlioImageMetaDevice.h"
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro(igtlioImageMetaDeviceCreator);
+vtkStandardNewMacro(igtlioImageMetaDevice);
+
+//---------------------------------------------------------------------------
+igtlioDevicePointer igtlioImageMetaDeviceCreator::Create(std::string device_name)
+{
+  igtlioImageMetaDevicePointer retval = igtlioImageMetaDevicePointer::New();
+  retval->SetDeviceName(device_name);
+  return retval;
+}
+
+//---------------------------------------------------------------------------
+std::string igtlioImageMetaDeviceCreator::GetDeviceType() const
+{
+  return igtlioImageMetaConverter::GetIGTLTypeName();
+}
+
+//---------------------------------------------------------------------------
+igtlioImageMetaDevice::igtlioImageMetaDevice()
+{
+}
+
+//---------------------------------------------------------------------------
+igtlioImageMetaDevice::~igtlioImageMetaDevice()
+{
+}
+
+//---------------------------------------------------------------------------
+std::string igtlioImageMetaDevice::GetDeviceType() const
+{
+  return igtlioImageMetaConverter::GetIGTLTypeName();
+}
+
+//---------------------------------------------------------------------------
+unsigned int igtlioImageMetaDevice::GetDeviceContentModifiedEvent() const
+{
+  return ImageMetaModifiedEvent;
+}
+
+//---------------------------------------------------------------------------
+int igtlioImageMetaDevice::ReceiveIGTLMessage(igtl::MessageBase::Pointer buffer, bool checkCRC)
+{
+  int success = igtlioImageMetaConverter::fromIGTL(buffer, &HeaderData, &Content, checkCRC, this->metaInfo);
+  if (success)
+  {
+    this->Modified();
+    this->InvokeEvent(this->GetDeviceContentModifiedEvent(), this);
+  }
+  return success;
+}
+
+//---------------------------------------------------------------------------
+igtl::MessageBase::Pointer igtlioImageMetaDevice::GetIGTLMessage()
+{
+  if (!igtlioImageMetaConverter::toIGTL(HeaderData, Content, &this->OutMessage, this->metaInfo))
+  {
+    return 0;
+  }
+
+  return dynamic_pointer_cast<igtl::MessageBase>(this->OutMessage);
+}
+
+//---------------------------------------------------------------------------
+igtl::MessageBase::Pointer igtlioImageMetaDevice::GetIGTLMessage(MESSAGE_PREFIX prefix)
+{
+  if (prefix == MESSAGE_PREFIX_NOT_DEFINED)
+  {
+    return this->GetIGTLMessage();
+  }
+  else if (prefix == MESSAGE_PREFIX_GET)
+  {
+    igtl::GetImageMetaMessage::Pointer getImageMetaMessage = igtl::GetImageMetaMessage::New();
+    getImageMetaMessage->SetDeviceName(this->GetDeviceName());
+    getImageMetaMessage->Pack();
+    return getImageMetaMessage;
+  }
+
+  return igtl::MessageBase::Pointer();
+}
+
+//---------------------------------------------------------------------------
+std::set<igtlioDevice::MESSAGE_PREFIX> igtlioImageMetaDevice::GetSupportedMessagePrefixes() const
+{
+  std::set<MESSAGE_PREFIX> retval;
+  retval.insert(MESSAGE_PREFIX_NOT_DEFINED);
+  retval.insert(MESSAGE_PREFIX_GET);
+  return retval;
+}
+
+//---------------------------------------------------------------------------
+void igtlioImageMetaDevice::SetContent(igtlioImageMetaConverter::ContentData content)
+{
+  Content = content;
+  this->Modified();
+  this->InvokeEvent(this->GetDeviceContentModifiedEvent(), this);
+}
+
+//---------------------------------------------------------------------------
+igtlioImageMetaConverter::ContentData igtlioImageMetaDevice::GetContent()
+{
+  return Content;
+}
+
+//---------------------------------------------------------------------------
+void igtlioImageMetaDevice::PrintSelf(ostream& os, vtkIndent indent)
+{
+  igtlioDevice::PrintSelf(os, indent);
+
+  os << indent << "ImageMetaElements:" << std::endl;
+  int i = 0;
+  for (igtlioImageMetaConverter::ImageMetaDataList::const_iterator imageMetaDataIt = this->Content.ImageMetaDataElements.begin();
+    imageMetaDataIt != Content.ImageMetaDataElements.end(); ++imageMetaDataIt)
+  {
+    os << indent << indent << "ImageMetaData #" << i << std::endl;
+    igtl::ImageMetaElement::Pointer imageMetaElement = igtl::ImageMetaElement::New();
+    os << indent << indent << "Name: " << imageMetaDataIt->Name << std::endl;
+    os << indent << indent << "DeviceName: " << imageMetaDataIt->DeviceName << std::endl;
+    os << indent << indent << "Modality: " << imageMetaDataIt->Modality << std::endl;
+    os << indent << indent << "PatientName: " << imageMetaDataIt->PatientName << std::endl;
+    os << indent << indent << "PatientID: " << imageMetaDataIt->PatientID << std::endl;
+    os << indent << indent << "Timestamp: " << imageMetaDataIt->Timestamp << std::endl;
+    os << indent << indent << "Size: " << imageMetaDataIt->Size[0] << ", " << imageMetaDataIt->Size[1] << ", " <<
+      imageMetaDataIt->Size[2] << std::endl;
+    os << indent << indent << "ScalarType: " << imageMetaDataIt->ScalarType << std::endl;
+  }
+
+}

Devices/igtlioPointDevice.cxx
@@ -1,0 +1,131 @@
+
+// OpenIGTLInkIO includes
+#include "igtlioPointDevice.h"
+
+// OpenIGTLink includes
+#include "igtlPointMessage.h"
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro(igtlioPointDeviceCreator);
+vtkStandardNewMacro(igtlioPointDevice);
+
+//---------------------------------------------------------------------------
+igtlioDevicePointer igtlioPointDeviceCreator::Create(std::string device_name)
+{
+  igtlioPointDevicePointer retval = igtlioPointDevicePointer::New();
+  retval->SetDeviceName(device_name);
+  return retval;
+}
+
+//---------------------------------------------------------------------------
+std::string igtlioPointDeviceCreator::GetDeviceType() const
+{
+  return igtlioPointConverter::GetIGTLTypeName();
+}
+
+//---------------------------------------------------------------------------
+igtlioPointDevice::igtlioPointDevice()
+{
+}
+
+//---------------------------------------------------------------------------
+igtlioPointDevice::~igtlioPointDevice()
+{
+}
+
+//---------------------------------------------------------------------------
+std::string igtlioPointDevice::GetDeviceType() const
+{
+  return igtlioPointConverter::GetIGTLTypeName();
+}
+
+//---------------------------------------------------------------------------
+unsigned int igtlioPointDevice::GetDeviceContentModifiedEvent() const
+{
+  return PointModifiedEvent;
+}
+
+//---------------------------------------------------------------------------
+int igtlioPointDevice::ReceiveIGTLMessage(igtl::MessageBase::Pointer buffer, bool checkCRC)
+{
+  int success = igtlioPointConverter::fromIGTL(buffer, &HeaderData, &Content, checkCRC, this->metaInfo);
+  if (success)
+  {
+    this->Modified();
+    this->InvokeEvent(this->GetDeviceContentModifiedEvent(), this);
+  }
+  return success;
+}
+
+//---------------------------------------------------------------------------
+igtl::MessageBase::Pointer igtlioPointDevice::GetIGTLMessage()
+{
+  if (!igtlioPointConverter::toIGTL(HeaderData, Content, &this->OutMessage, this->metaInfo))
+  {
+    return 0;
+  }
+
+  return dynamic_pointer_cast<igtl::MessageBase>(this->OutMessage);
+}
+
+//---------------------------------------------------------------------------
+igtl::MessageBase::Pointer igtlioPointDevice::GetIGTLMessage(MESSAGE_PREFIX prefix)
+{
+  if (prefix == MESSAGE_PREFIX_NOT_DEFINED)
+  {
+    return this->GetIGTLMessage();
+  }
+  else if (prefix == MESSAGE_PREFIX_GET)
+  {
+    igtl::GetPointMessage::Pointer getPointMessage = igtl::GetPointMessage::New();
+    getPointMessage->SetDeviceName(this->GetDeviceName());
+    getPointMessage->Pack();
+    return getPointMessage;
+  }
+
+  return igtl::MessageBase::Pointer();
+}
+
+//---------------------------------------------------------------------------
+std::set<igtlioDevice::MESSAGE_PREFIX> igtlioPointDevice::GetSupportedMessagePrefixes() const
+{
+  std::set<MESSAGE_PREFIX> retval;
+  retval.insert(MESSAGE_PREFIX_NOT_DEFINED);
+  retval.insert(MESSAGE_PREFIX_GET);
+  return retval;
+}
+
+//---------------------------------------------------------------------------
+void igtlioPointDevice::SetContent(igtlioPointConverter::ContentData content)
+{
+  Content = content;
+  this->Modified();
+  this->InvokeEvent(this->GetDeviceContentModifiedEvent(), this);
+}
+
+//---------------------------------------------------------------------------
+igtlioPointConverter::ContentData igtlioPointDevice::GetContent()
+{
+  return Content;
+}
+
+//---------------------------------------------------------------------------
+void igtlioPointDevice::PrintSelf(ostream& os, vtkIndent indent)
+{
+  igtlioDevice::PrintSelf(os, indent);
+  os << indent << "Points:" << std::endl;
+  int i = 0;
+  for (igtlioPointConverter::PointList::const_iterator pointIt = this->Content.PointElements.begin();
+    pointIt != Content.PointElements.end(); ++pointIt)
+  {
+    os << indent << indent << "Point #" << i << std::endl;
+    igtl::PointElement::Pointer pointElement = igtl::PointElement::New();
+    os << indent << indent << "Name: " << pointIt->Name << std::endl;
+    os << indent << indent << "GroupName: " << pointIt->GroupName << std::endl;
+    os << indent << indent << "Position: " << pointIt->Position[0] << ", " << pointIt->Position[1] << ", "
+      << pointIt->Position[2] << std::endl;
+    os << indent << indent << "Radius: " << pointIt->Radius << std::endl;
+    os << indent << indent << "Owner: " << pointIt->Owner << std::endl;
+    ++i;
+  }
+}

Converter/CMakeLists.txt
@@ -20,24 +20,31 @@ set(${PROJECT_NAME}_INCLUDE_DIRECTORIES PUBLIC
   )
 set(${PROJECT_NAME}_SRCS
   igtlioBaseConverter.cxx
-  igtlioImageConverter.cxx
-  igtlioPolyDataConverter.cxx
-  igtlioStatusConverter.cxx
-  igtlioTransformConverter.cxx
   igtlioCommandConverter.cxx
-  igtlioStringConverter.cxx
   igtlioConverterUtilities.cxx
+  igtlioImageConverter.cxx
+  igtlioImageMetaConverter.cxx
+  igtlioLabelMetaConverter.cxx
+  igtlioPolyDataConverter.cxx
+  igtlioPointConverter.cxx
+  igtlioStatusConverter.cxx
+  igtlioStringConverter.cxx
+  igtlioTransformConverter.cxx
   )
 
 set(${PROJECT_NAME}_HDRS
   igtlioBaseConverter.h
-  igtlioImageConverter.h
-  igtlioPolyDataConverter.h
-  igtlioStatusConverter.h
-  igtlioTransformConverter.h
-  igtlioStringConverter.h
-  igtlioUsSectorDefinitions.h
+  igtlioCommandConverter.h
   igtlioConverterUtilities.h
+  igtlioImageConverter.h
+  igtlioImageMetaConverter.h
+  igtlioLabelMetaConverter.h
+  igtlioPolyDataConverter.h
+  igtlioPointConverter.h
+  igtlioStatusConverter.h
+  igtlioStringConverter.h
+  igtlioTransformConverter.h
+  igtlioUsSectorDefinitions.h
   )
 
 IF(OpenIGTLink_ENABLE_VIDEOSTREAMING)
@@ -62,7 +69,7 @@ generate_export_header(${PROJECT_NAME} EXPORT_FILE_NAME ${PROJECT_NAME}Export.h 
 
 # --------------------------------------------------------------------------
 
-INSTALL(TARGETS ${PROJECT_NAME} EXPORT OpenIGTLinkIO 
+INSTALL(TARGETS ${PROJECT_NAME} EXPORT OpenIGTLinkIO
   RUNTIME DESTINATION "${OpenIGTLinkIO_BINARY_INSTALL}" COMPONENT RuntimeLibraries
   LIBRARY DESTINATION "${OpenIGTLinkIO_LIBRARY_INSTALL}" COMPONENT RuntimeLibraries
   ARCHIVE DESTINATION "${OpenIGTLinkIO_ARCHIVE_INSTALL}" COMPONENT Development

Converter/igtlioImageMetaConverter.cxx
@@ -1,0 +1,101 @@
+
+// OpenIGTLinkIO includes
+#include "igtlioImageMetaConverter.h"
+
+// OpenIGTLink includes
+#include <igtl_util.h>
+#include <igtlImageMetaMessage.h>
+
+//---------------------------------------------------------------------------
+int igtlioImageMetaConverter::fromIGTL(igtl::MessageBase::Pointer source,
+  HeaderData* header,
+  ContentData* dest,
+  bool checkCRC,
+  igtl::MessageBase::MetaDataMap& outMetaInfo)
+{
+  // Create a message buffer to receive  data
+  igtl::ImageMetaMessage::Pointer msg;
+  msg = igtl::ImageMetaMessage::New();
+  msg->Copy(source); // !! TODO: copy makes performance issue.
+
+  // Deserialize the data
+  // If CheckCRC==0, CRC check is skipped.
+  int c = msg->Unpack(checkCRC);
+  if ((c & igtl::MessageHeader::UNPACK_BODY) == 0) // if CRC check fails
+  {
+    // TODO: error handling
+    return 0;
+  }
+
+  // get header
+  if (!IGTLtoHeader(dynamic_pointer_cast<igtl::MessageBase>(msg), header, outMetaInfo))
+  {
+    return 0;
+  }
+
+  dest->ImageMetaDataElements.clear();
+  int nElements = msg->GetNumberOfImageMetaElement();
+  for (int i = 0; i < nElements; i++)
+  {
+    igtl::ImageMetaElement::Pointer imgMetaElement;
+    msg->GetImageMetaElement(i, imgMetaElement);
+
+    igtlUint16 size[3];
+    imgMetaElement->GetSize(size);
+
+    igtl::TimeStamp::Pointer ts;
+    imgMetaElement->GetTimeStamp(ts);
+    double time = ts->GetTimeStamp();
+
+    ImageMetaElement element;
+    element.Name = imgMetaElement->GetName();
+    element.DeviceName = imgMetaElement->GetDeviceName();
+    element.Modality = imgMetaElement->GetModality();
+    element.PatientName = imgMetaElement->GetPatientName();
+    element.PatientID = imgMetaElement->GetPatientID();
+    element.Timestamp = time;
+    element.Size[0] = size[0];
+    element.Size[1] = size[1];
+    element.Size[2] = size[2];
+    element.ScalarType = imgMetaElement->GetScalarType();
+    dest->ImageMetaDataElements.push_back(element);
+  }
+
+  return 1;
+}
+
+//---------------------------------------------------------------------------
+int igtlioImageMetaConverter::toIGTL(const HeaderData& header, const ContentData& source, igtl::ImageMetaMessage::Pointer* dest, igtl::MessageBase::MetaDataMap metaInfo)
+{
+  if (dest->IsNull())
+  {
+    *dest = igtl::ImageMetaMessage::New();
+  }
+  (*dest)->InitPack();
+  igtl::ImageMetaMessage::Pointer msg = *dest;
+
+  if (!metaInfo.empty())
+  {
+    msg->SetHeaderVersion(IGTL_HEADER_VERSION_2);
+  }
+  igtl::MessageBase::Pointer basemsg = dynamic_pointer_cast<igtl::MessageBase>(msg);
+  HeadertoIGTL(header, &basemsg, metaInfo);
+
+  for (ImageMetaDataList::const_iterator imageMetaDataIt = source.ImageMetaDataElements.begin(); imageMetaDataIt != source.ImageMetaDataElements.end(); ++imageMetaDataIt)
+  {
+    igtl::ImageMetaElement::Pointer imageMetaElement = igtl::ImageMetaElement::New();
+    imageMetaElement->SetName(imageMetaDataIt->Name.c_str());
+    imageMetaElement->SetDeviceName(imageMetaDataIt->DeviceName.c_str());
+    imageMetaElement->SetModality(imageMetaDataIt->Modality.c_str());
+    imageMetaElement->SetPatientName(imageMetaDataIt->PatientName.c_str());
+    imageMetaElement->SetPatientID(imageMetaDataIt->PatientID.c_str());
+    igtl::TimeStamp::Pointer timestamp = igtl::TimeStamp::New();
+    timestamp->SetTime(imageMetaDataIt->Timestamp);
+    imageMetaElement->SetTimeStamp(timestamp);
+    imageMetaElement->SetSize(imageMetaDataIt->Size[0], imageMetaDataIt->Size[1], imageMetaDataIt->Size[2]);
+    imageMetaElement->SetScalarType(imageMetaDataIt->ScalarType);
+    msg->AddImageMetaElement(imageMetaElement);
+  }
+
+  return 1;
+}

Converter/igtlioImageMetaConverter.h
@@ -1,0 +1,43 @@
+#ifndef IMAGEMETACONVERTER_H
+#define IMAGEMETACONVERTER_H
+
+// OpenIGTLinkIO includes
+#include "igtlioBaseConverter.h"
+#include "igtlioConverterExport.h"
+
+// OpenIGTLink includes
+#include <igtlImageMetaMessage.h>
+
+class OPENIGTLINKIO_CONVERTER_EXPORT igtlioImageMetaConverter : public igtlioBaseConverter
+{
+public:
+  /**
+   * Content of STRING message.
+   */
+  struct ImageMetaElement
+  {
+    std::string   Name;        /* name / description (< 64 bytes)*/
+    std::string   DeviceName;  /* device name to query the IMAGE and COLORT */
+    std::string   Modality;    /* modality name (< 32 bytes) */
+    std::string   PatientName; /* patient name (< 64 bytes) */
+    std::string   PatientID;   /* patient ID (MRN etc.) (< 64 bytes) */
+    double        Timestamp;   /* scan time */
+    int           Size[3];     /* entire image volume size */
+    unsigned char ScalarType;  /* scalar type. see scalar_type in IMAGE message */
+  };
+  typedef std::vector<ImageMetaElement> ImageMetaDataList;
+
+  struct ContentData
+  {
+    ImageMetaDataList ImageMetaDataElements;
+  };
+
+  static const char*  GetIGTLName() { return GetIGTLTypeName(); }
+  static const char* GetIGTLTypeName() { return "IMGMETA"; }
+
+  static int fromIGTL(igtl::MessageBase::Pointer source, HeaderData* header, ContentData* content, bool checkCRC, igtl::MessageBase::MetaDataMap& outMetaInfo);
+  static int toIGTL(const HeaderData& header, const ContentData& source, igtl::ImageMetaMessage::Pointer* dest, igtl::MessageBase::MetaDataMap metaInfo = igtl::MessageBase::MetaDataMap());
+
+};
+
+#endif // IMAGEMETACONVERTER_H

Converter/igtlioLabelMetaConverter.cxx
@@ -1,0 +1,96 @@
+
+// OpenIGTLInkIO includes
+#include "igtlioLabelMetaConverter.h"
+
+// OpenIGTLink includes
+#include <igtl_util.h>
+#include <igtlLabelMetaMessage.h>
+
+//---------------------------------------------------------------------------
+int igtlioLabelMetaConverter::fromIGTL(igtl::MessageBase::Pointer source,
+  HeaderData* header,
+  ContentData* dest,
+  bool checkCRC,
+  igtl::MessageBase::MetaDataMap& outMetaInfo)
+{
+  // Create a message buffer to receive  data
+  igtl::LabelMetaMessage::Pointer msg;
+  msg = igtl::LabelMetaMessage::New();
+  msg->Copy(source); // !! TODO: copy makes performance issue.
+
+  // Deserialize the data
+  // If CheckCRC==0, CRC check is skipped.
+  int c = msg->Unpack(checkCRC);
+  if ((c & igtl::MessageHeader::UNPACK_BODY) == 0) // if CRC check fails
+  {
+    // TODO: error handling
+    return 0;
+  }
+
+  // get header
+  if (!IGTLtoHeader(dynamic_pointer_cast<igtl::MessageBase>(msg), header, outMetaInfo))
+  {
+    return 0;
+  }
+
+  dest->LabelMetaDataElements.clear();
+  int nElements = msg->GetNumberOfLabelMetaElement();
+  for (int i = 0; i < nElements; i++)
+  {
+    igtl::LabelMetaElement::Pointer lbMetaElement;
+    msg->GetLabelMetaElement(i, lbMetaElement);
+
+    igtlUint16 size[3];
+    lbMetaElement->GetSize(size);
+
+    LabelMetaElement element;
+    element.Name = lbMetaElement->GetName();
+    element.DeviceName = lbMetaElement->GetDeviceName();
+    element.Label = lbMetaElement->GetLabel();
+    unsigned char r,b,g,a = 0;
+    lbMetaElement->GetRGBA(r,g,b,a);
+    element.RGBA[0] = r;
+    element.RGBA[1] = g;
+    element.RGBA[2] = b;
+    element.RGBA[3] = a;
+    element.Size[0] = size[0];
+    element.Size[1] = size[1];
+    element.Size[2] = size[2];
+    element.Owner = lbMetaElement->GetOwner();
+    dest->LabelMetaDataElements.push_back(element);
+  }
+
+  return 1;
+}
+
+//---------------------------------------------------------------------------
+int igtlioLabelMetaConverter::toIGTL(const HeaderData& header, const ContentData& source, igtl::LabelMetaMessage::Pointer* dest, igtl::MessageBase::MetaDataMap metaInfo)
+{
+  if (dest->IsNull())
+  {
+    *dest = igtl::LabelMetaMessage::New();
+  }
+  (*dest)->InitPack();
+  igtl::LabelMetaMessage::Pointer msg = *dest;
+
+  if (!metaInfo.empty())
+  {
+    msg->SetHeaderVersion(IGTL_HEADER_VERSION_2);
+  }
+  igtl::MessageBase::Pointer basemsg = dynamic_pointer_cast<igtl::MessageBase>(msg);
+  HeadertoIGTL(header, &basemsg, metaInfo);
+
+  for (LabelMetaDataList::const_iterator labelMetaDataIt = source.LabelMetaDataElements.begin(); labelMetaDataIt != source.LabelMetaDataElements.end(); ++labelMetaDataIt)
+  {
+    igtl::LabelMetaElement::Pointer labelMetaElement = igtl::LabelMetaElement::New();
+    labelMetaElement->SetName(labelMetaDataIt->Name.c_str());
+    labelMetaElement->SetDeviceName(labelMetaDataIt->DeviceName.c_str());
+    labelMetaElement->SetLabel(labelMetaDataIt->Label);
+    labelMetaElement->SetRGBA(labelMetaDataIt->RGBA[0], labelMetaDataIt->RGBA[1], labelMetaDataIt->RGBA[2], labelMetaDataIt->RGBA[3]);
+    labelMetaElement->SetSize(labelMetaDataIt->Size[0], labelMetaDataIt->Size[1], labelMetaDataIt->Size[2]);
+    labelMetaElement->SetOwner(labelMetaDataIt->Owner.c_str());
+    msg->AddLabelMetaElement(labelMetaElement);
+  }
+
+  return 1;
+}

Converter/igtlioLabelMetaConverter.h
@@ -1,0 +1,42 @@
+#ifndef LABELMETACONVERTER_H
+#define LABELMETACONVERTER_H
+
+// OpenIGTLinkIO includes
+#include "igtlioBaseConverter.h"
+#include "igtlioConverterExport.h"
+
+// OpenIGTLink includes
+#include <igtlLabelMetaMessage.h>
+
+class OPENIGTLINKIO_CONVERTER_EXPORT igtlioLabelMetaConverter : public igtlioBaseConverter
+{
+public:
+  /**
+   * Content of STRING message.
+   */
+  struct LabelMetaElement
+  {
+    std::string   Name;        /* Name or description of the image */
+    std::string   DeviceName;  /* ID to query the IMAGE */
+    unsigned char Label;       /* Label of the structure (0 if unused) */
+    unsigned char RGBA[4];     /* Color in RGBA (0 0 0 0 if no color is defined) */
+    int           Size[3];     /* Number of pixels in each direction (same as in IMAGE), bounding box of the structure(s) */
+    std::string   Owner;       /* ID of the owner image/sliceset. Voxel objects from different slicesets can be sent if
+                               slicesets are fused. Can be empty if n/a */
+  };
+  typedef std::vector<LabelMetaElement> LabelMetaDataList;
+
+  struct ContentData
+  {
+    LabelMetaDataList LabelMetaDataElements;
+  };
+
+  static const char*  GetIGTLName() { return GetIGTLTypeName(); }
+  static const char* GetIGTLTypeName() { return "LBMETA"; }
+
+  static int fromIGTL(igtl::MessageBase::Pointer source, HeaderData* header, ContentData* content, bool checkCRC, igtl::MessageBase::MetaDataMap& outMetaInfo);
+  static int toIGTL(const HeaderData& header, const ContentData& source, igtl::LabelMetaMessage::Pointer* dest, igtl::MessageBase::MetaDataMap metaInfo = igtl::MessageBase::MetaDataMap());
+
+};
+
+#endif // IMAGEMETACONVERTER_H

Converter/igtlioPointConverter.cxx
@@ -1,0 +1,96 @@
+
+// OpenIGTLinkIO incldues
+#include "igtlioPointConverter.h"
+
+// OpenIGTLink includes
+#include <igtl_util.h>
+#include <igtlPointMessage.h>
+
+//---------------------------------------------------------------------------
+int igtlioPointConverter::fromIGTL(igtl::MessageBase::Pointer source,
+  HeaderData* header,
+  ContentData* dest,
+  bool checkCRC,
+  igtl::MessageBase::MetaDataMap& outMetaInfo)
+{
+  // Create a message buffer to receive  data
+  igtl::PointMessage::Pointer msg;
+  msg = igtl::PointMessage::New();
+  msg->Copy(source); // !! TODO: copy makes performance issue.
+
+  // Deserialize the data
+  // If CheckCRC==0, CRC check is skipped.
+  int c = msg->Unpack(checkCRC);
+  if ((c & igtl::MessageHeader::UNPACK_BODY) == 0) // if CRC check fails
+  {
+    // TODO: error handling
+    return 0;
+  }
+
+  // get header
+  if (!IGTLtoHeader(dynamic_pointer_cast<igtl::MessageBase>(msg), header, outMetaInfo))
+  {
+    return 0;
+  }
+
+  dest->PointElements.clear();
+  int nElements = msg->GetNumberOfPointElement();
+  for (int i = 0; i < nElements; i++)
+  {
+    igtl::PointElement::Pointer pointElement;
+    msg->GetPointElement(i, pointElement);
+
+    PointElement element;
+    element.Name = pointElement->GetName();
+    element.GroupName = pointElement->GetGroupName();
+    unsigned char r,b,g,a = 0;
+    pointElement->GetRGBA(r,g,b,a);
+    element.RGBA[0] = r;
+    element.RGBA[1] = g;
+    element.RGBA[2] = b;
+    element.RGBA[3] = a;
+    float position[3] = { 0,0,0 };
+    pointElement->GetPosition(position);
+    for (int i = 0; i < 3; ++i)
+    {
+      element.Position[i] = position[i];
+    }
+    element.Radius = pointElement->GetRadius();
+    element.Owner = pointElement->GetOwner();
+    dest->PointElements.push_back(element);
+  }
+
+  return 1;
+}
+
+//---------------------------------------------------------------------------
+int igtlioPointConverter::toIGTL(const HeaderData& header, const ContentData& source, igtl::PointMessage::Pointer* dest, igtl::MessageBase::MetaDataMap metaInfo)
+{
+  if (dest->IsNull())
+  {
+    *dest = igtl::PointMessage::New();
+  }
+  (*dest)->InitPack();
+  igtl::PointMessage::Pointer msg = *dest;
+
+  if (!metaInfo.empty())
+  {
+    msg->SetHeaderVersion(IGTL_HEADER_VERSION_2);
+  }
+  igtl::MessageBase::Pointer basemsg = dynamic_pointer_cast<igtl::MessageBase>(msg);
+  HeadertoIGTL(header, &basemsg, metaInfo);
+
+  for (PointList::const_iterator pointIt = source.PointElements.begin(); pointIt != source.PointElements.end(); ++pointIt)
+  {
+    igtl::PointElement::Pointer pointElement = igtl::PointElement::New();
+    pointElement->SetName(pointIt->Name.c_str());
+    pointElement->SetGroupName(pointIt->GroupName.c_str());
+    pointElement->SetRGBA(pointIt->RGBA[0], pointIt->RGBA[1], pointIt->RGBA[2], pointIt->RGBA[3]);
+    pointElement->SetPosition(pointIt->Position[0], pointIt->Position[1], pointIt->Position[2]);
+    pointElement->SetRadius(pointIt->Radius);
+    pointElement->SetOwner(pointIt->Owner.c_str());
+    msg->AddPointElement(pointElement);
+  }
+
+  return 1;
+}

commit 5effea38209c71604c1409c5e21d81bdb09ca6fe

Fix Linux build error
Introduced in 53928bb559d4fb78c0e724b90f858cb7f8c2dc1f

Devices/igtlioImageMetaDevice.cxx
@@ -77,7 +77,7 @@ igtl::MessageBase::Pointer igtlioImageMetaDevice::GetIGTLMessage(MESSAGE_PREFIX 
     igtl::GetImageMetaMessage::Pointer getImageMetaMessage = igtl::GetImageMetaMessage::New();
     getImageMetaMessage->SetDeviceName(this->GetDeviceName());
     getImageMetaMessage->Pack();
-    return getImageMetaMessage;
+    return dynamic_pointer_cast<igtl::MessageBase>(getImageMetaMessage);
   }
 
   return igtl::MessageBase::Pointer();

Devices/igtlioPointDevice.cxx
@@ -80,7 +80,7 @@ igtl::MessageBase::Pointer igtlioPointDevice::GetIGTLMessage(MESSAGE_PREFIX pref
     igtl::GetPointMessage::Pointer getPointMessage = igtl::GetPointMessage::New();
     getPointMessage->SetDeviceName(this->GetDeviceName());
     getPointMessage->Pack();
-    return getPointMessage;
+    return dynamic_pointer_cast<igtl::MessageBase>(getPointMessage);
   }
 
   return igtl::MessageBase::Pointer();